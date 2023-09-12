4aff043f945cd7f3e2d3e06696a0a4013e02dd46
Leonardo Betetto <60801645+lmbbetetto@users.noreply.github.com>
2023-09-12 14:49:10 -0300

    commit

--- src/components/homePage/homePage.tsx
+++ src/components/homePage/homePage.tsx
@@ -1,22 +1,23 @@
-import { Card } from './card/card'
+// import { Card } from './card/card'
 import style from './style.module.css'
 
 import Australia from '../../assets/bandeiraAustralia.png'
 import NZ from '../../assets/bandeiraNZ.png'
 import { useEffect } from 'react'
+import { Link } from 'react-router-dom'
 
-const Cards = [
-    {
-        img: Australia,
-        name: 'Australia',
-        link: '/australia',
-    },
-    {
-        img: NZ,
-        name: 'Nova Zelândia',
-        link: '/novazelandia',
-    },
-]
+// const Cards = [
+//     {
+//         img: Australia,
+//         name: 'Australia',
+//         link: '/australia',
+//     },
+//     {
+//         img: NZ,
+//         name: 'Nova Zelândia',
+//         link: '/novazelandia',
+//     },
+// ]
 
 export function Home() {
     useEffect(() => {
@@ -28,7 +29,7 @@ export function Home() {
             <div className={style.cover}></div>
 
             <div className={style.container}>
-                {
+                {/* {
                     Cards.map((component, index) => (
                         <Card
                             key={index}
@@ -37,7 +38,20 @@ export function Home() {
                             link={component.link}
                         />
                     ))
-                }
+                } */}
+                <div className={style.card}>
+                    <img src={Australia} alt="bandeira" />
+                    <h1 className={style.title}>Australia</h1>
+                    <p className={style.paragraph}>Mais informações</p>
+                    <Link to={"/australia"}><button className={style.btn}>Acessar</button></Link>
+                </div>
+
+                <div className={style.cardNZ}>
+                    <img src={NZ} alt="bandeira" />
+                    <h1 className={style.title}>Nova Zelândia</h1>
+                    <p className={style.paragraph}>Mais informações</p>
+                    <Link to={"/novazelandia"}><button className={style.btn}>Acessar</button></Link>
+                </div>
             </div>
         </>
     )
