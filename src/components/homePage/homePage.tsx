5495834e75afc41a69f48fdf13e64eb63b8bae1f
Leonardo Betetto <60801645+lmbbetetto@users.noreply.github.com>
2023-09-13 14:14:16 -0300

    commit Kiwi Bird

--- src/components/homePage/homePage.tsx
+++ src/components/homePage/homePage.tsx
@@ -1,23 +1,9 @@
-// import { Card } from './card/card'
 import style from './style.module.css'
 
 import Australia from '../../assets/bandeiraAustralia.png'
 import NZ from '../../assets/bandeiraNZ.png'
 import { useEffect } from 'react'
 import { Link } from 'react-router-dom'
-
-// const Cards = [
-//     {
-//         img: Australia,
-//         name: 'Australia',
-//         link: '/australia',
-//     },
-//     {
-//         img: NZ,
-//         name: 'Nova Zelândia',
-//         link: '/novazelandia',
-//     },
-// ]
 
 export function Home() {
     useEffect(() => {
@@ -29,16 +15,6 @@ export function Home() {
             <div className={style.cover}></div>
 
             <div className={style.container}>
-                {/* {
-                    Cards.map((component, index) => (
-                        <Card
-                            key={index}
-                            img={component.img}
-                            name={component.name}
-                            link={component.link}
-                        />
-                    ))
-                } */}
                 <div className={style.card}>
                     <img src={Australia} alt="bandeira" />
                     <h1 className={style.title}>Australia</h1>
